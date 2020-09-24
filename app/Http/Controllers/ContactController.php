583cf1d997471135e93a6e45926efeb49d0b31bb
ubuntu <ubuntu@ubuntu-System-Product-Name>
2020-09-24 19:09:28 +0500

    Saving data from feedback to table

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,15 +2,21 @@
 
 namespace App\Http\Controllers;
 
-
 use Illuminate\Http\Request;
 use App\Http\Requests\ContactRequest;
+use App\Contact;
 
-class ContactController extends Controller
-{
-    public function submit(ContactRequest $request){
+class ContactController extends Controller {
+
+    public function submit(ContactRequest $request) {
         //dd($request);
-   
-       
+        $contact = new Contact();
+        $contact->name = $request->input('name');
+        $contact->subject = $request->input('subject');
+        $contact->email = $request->input('email');
+        $contact->textarea = $request->input('textarea');        
+        $contact->save();// save form to table contacts
+        return redirect('/feedback');
     }
+
 }
